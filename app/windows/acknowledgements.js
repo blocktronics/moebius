d2a744d95ae4b5c6d7d3a466cdc447d34bfa79c0
Andy Herbert <andy.herbert@gmail.com>
2020-05-14 06:00:25 +0100

    Swap out acknowledgement ansis

--- app/windows/acknowledgements.js
+++ app/windows/acknowledgements.js
@@ -6,5 +6,5 @@ const electron = require("electron");
 document.addEventListener("keydown", (event) => electron.remote.getCurrentWindow().close(), true);
 
 document.addEventListener("DOMContentLoaded", () => {
-    libtextmode.animate({file: `${ans_path}acknowledgements.ans`, ctx: document.getElementById("acknowledgements_terminal").getContext("2d")});
+    libtextmode.animate({file: `${ans_path}anst-moebius.ans`, ctx: document.getElementById("acknowledgements_terminal").getContext("2d")});
 });
