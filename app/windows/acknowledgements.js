6932b70b8b83a90973ebedd4394b33b0baee40c1
Andy Herbert <andy.herbert@gmail.com>
2020-05-14 06:04:39 +0100

    Wait for escape key to be pressed before dismissing windows

--- app/windows/acknowledgements.js
+++ app/windows/acknowledgements.js
@@ -3,7 +3,9 @@ const dev = require("electron-is-dev");
 const ans_path = dev ? "./build/ans/" : `${process.resourcesPath}/ans/`;
 const electron = require("electron");
 
-document.addEventListener("keydown", (event) => electron.remote.getCurrentWindow().close(), true);
+document.addEventListener("keydown", (event) => {
+    if (event.key == "Escape") electron.remote.getCurrentWindow().close();
+}, true);
 
 document.addEventListener("DOMContentLoaded", () => {
     libtextmode.animate({file: `${ans_path}anst-moebius.ans`, ctx: document.getElementById("acknowledgements_terminal").getContext("2d")});
--- app/windows/numpad_mappings.js
+++ app/windows/numpad_mappings.js
@@ -3,7 +3,9 @@ const dev = require("electron-is-dev");
 const ans_path = dev ? "./build/ans/" : `${process.resourcesPath}/ans/`;
 const electron = require("electron");
 
-document.addEventListener("keydown", (event) => electron.remote.getCurrentWindow().close(), true);
+document.addEventListener("keydown", (event) => {
+    if (event.key == "Escape") electron.remote.getCurrentWindow().close();
+}, true);
 
 document.addEventListener("DOMContentLoaded", () => {
     libtextmode.animate({file: `${ans_path}numpad_mappings.ans`, ctx: document.getElementById("numpad_mappings_terminal").getContext("2d")});
